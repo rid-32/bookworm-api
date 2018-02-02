1fb747ea3c43df237a8e9300ad3b18533055809c
RiD_32 <rostokin.den@yandex.ru>
2018-02-02 20:44:15 +0300

    User authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+// bodyParset парсит приходящие от клиента пост запросом данные
+import bodyParser from 'body-parser';
+// dotenv парсит файл .env и значениями из этого файла заполняет process.env
+import dotenv from 'dotenv';
+// Promis-библиотека для работы с mongoose
+import Promise from 'bluebird';
+
+// Routes
+import auth from './routes/auth';
+
+// Парсим файл .env и заполняем process.evn
+dotenv.config();
 
 const app = express();
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+// Определяем Promis для mongoose
+mongoose.Promise = Promise;
+// Подключение к MongoDB
+mongoose.connect(process.env.MONGODB_URL);
+let db = mongoose.connection;
+db.on('open', () => console.log('We`re connected to the batabase: bookworm'));
+db.on('error', () => console.log('Something wrong!'));
+
+// Парсить json-данные, приходящие от клиента
+app.use(bodyParser.json());
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+// Модель пользователя для базы данных
+// С помощью этой модели данные сохраняются в коллекцию users
+
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// Схема модели пользователя
+const schema = new Schema(
+  {
+    email: { type: String, required: true, lowercase: true, index: true },
+    passwordHash: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+
+schema.methods.generateJWT = function generateJWT() {
+  return jwt.sign(
+    {
+      email: this.email
+    },
+    process.env.JWT_SECRET
+  );
+}
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    email: this.email,
+    token: this.generateJWT()
+  }
+}
+
+export default mongoose.model('User', schema);
